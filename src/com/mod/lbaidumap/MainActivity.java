5f7c9510a56738f1634321d84ac816bf72c550d3
Devil Star <devil.star.mod@gmail.com>
2015-03-13 18:06:04 +0800

    显示路径

--- src/com/mod/lbaidumap/MainActivity.java
+++ src/com/mod/lbaidumap/MainActivity.java
@@ -7,7 +7,12 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -16,6 +21,8 @@ import com.baidu.location.LocationClientOption;
 import com.baidu.location.LocationClientOption.LocationMode;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -23,8 +30,21 @@ import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.UiSettings;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.overlayutil.DrivingRouteOverlay;
+import com.baidu.mapapi.overlayutil.OverlayManager;
+import com.baidu.mapapi.overlayutil.WalkingRouteOverlay;
+import com.baidu.mapapi.search.core.RouteLine;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.route.DrivingRouteResult;
+import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
+import com.baidu.mapapi.search.route.PlanNode;
+import com.baidu.mapapi.search.route.RoutePlanSearch;
+import com.baidu.mapapi.search.route.TransitRouteResult;
+import com.baidu.mapapi.search.route.WalkingRoutePlanOption;
+import com.baidu.mapapi.search.route.WalkingRouteResult;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+		OnGetRoutePlanResultListener {
 	private static final String LTAG = MainActivity.class.getSimpleName();
 	private MapView mMapView;
 	private BaiduMap mBaiduMap;
@@ -32,6 +52,11 @@ public class MainActivity extends Activity {
 	private LocationClient mLocClient;
 	private SDKReceiver mReceiver;
 	private boolean isFirstLoc = true;
+	private EditText edtStart, edtEnd;
+	private Button btnSearch;
+	private RoutePlanSearch mSearch;
+	private RouteLine route;
+	private OverlayManager routeOverlay;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +68,16 @@ public class MainActivity extends Activity {
 	}
 
 	private void init() {
+		edtStart = (EditText) findViewById(R.id.edt_start);
+		edtEnd = (EditText) findViewById(R.id.edt_end);
+		btnSearch = (Button) findViewById(R.id.btn_search);
+		btnSearch.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				baiduRoutePlanSearch();
+			}
+		});
 		mMapView = (MapView) findViewById(R.id.bmapview);
 		mBaiduMap = mMapView.getMap();
 		mUiSettings = mBaiduMap.getUiSettings();
@@ -93,6 +128,20 @@ public class MainActivity extends Activity {
 	}
 
 	/**
+	 * 百度路线搜索
+	 */
+	private void baiduRoutePlanSearch() {
+		mSearch = RoutePlanSearch.newInstance();
+		mSearch.setOnGetRoutePlanResultListener(this);
+		PlanNode stNode = PlanNode.withCityNameAndPlaceName("广州", edtStart
+				.getText().toString());
+		PlanNode enNode = PlanNode.withCityNameAndPlaceName("广州", edtEnd
+				.getText().toString());
+		mSearch.walkingSearch(new WalkingRoutePlanOption().from(stNode).to(
+				enNode));
+	}
+
+	/**
 	 * 构造广播监听类，监听 SDK key 验证以及网络异常广播
 	 */
 	public class SDKReceiver extends BroadcastReceiver {
@@ -132,5 +181,39 @@ public class MainActivity extends Activity {
 	protected void onPause() {
 		mMapView.onPause();
 		super.onPause();
+	}
+
+	@Override
+	public void onGetDrivingRouteResult(DrivingRouteResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onGetTransitRouteResult(TransitRouteResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onGetWalkingRouteResult(WalkingRouteResult result) {
+		 if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+	            Toast.makeText(this, "抱歉，未找到结果", Toast.LENGTH_SHORT).show();
+	        }
+	        if (result.error == SearchResult.ERRORNO.AMBIGUOUS_ROURE_ADDR) {
+	            //起终点或途经点地址有岐义，通过以下接口获取建议查询信息
+	            //result.getSuggestAddrInfo()
+	            return;
+	        }
+	        if (result.error == SearchResult.ERRORNO.NO_ERROR) {
+	           
+	            route = result.getRouteLines().get(0);
+	            WalkingRouteOverlay overlay = new WalkingRouteOverlay(mBaiduMap);
+	            mBaiduMap.setOnMarkerClickListener(overlay);
+	            routeOverlay = overlay;
+	            overlay.setData(result.getRouteLines().get(0));
+	            overlay.addToMap();
+	            overlay.zoomToSpan();
+	        }
 	}
 }
